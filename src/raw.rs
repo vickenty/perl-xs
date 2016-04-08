4c8e03da5e2bde98bc1816abb2c8c074669b65d4
Vickenty Fesunov <kent@setattr.net>
2016-04-08 23:19:53 +0200

    Pass exceptions from Perl to Rust.
    This captures Perl exceptions on Perl-Rust boundary and converts them
    into panics of a specific type. These then can be captured later on
    Rust-Perl boundary and re-thrown again as Perl exceptions.

--- src/raw.rs
+++ src/raw.rs
@@ -1,5 +1,7 @@
 //! Minimal wrapper around internal Perl API.
 
+use std::mem;
+use std::os::raw::{ c_int };
 use perl_sys::funcs::*;
 
 pub use perl_sys::types::*;
@@ -12,7 +14,77 @@ pub struct Interpreter (PerlThreadContext);
 
 impl Interpreter {
     pub unsafe fn new(pthx: PerlThreadContext) -> Interpreter { Interpreter(pthx) }
+}
 
+// Contains result of failed JMPENV_PUSH() while propagating through Rust.
+struct Xcpt(c_int);
+
+// Wrapper to safely call potentially dying perl functions (which is pretty much all of them because
+// magic and ties).
+//
+// Put actual call into Perl inside a closure. `ouroboros_xcpt_try()` will call the closure while
+// capturing Perl exceptions, so they don't skip Rust code except the closure itself, which should
+// be fine, since closure is FnMut and has no destructors.
+//
+// If it returns zero, we assume that closure did not die and `v` was assigned to, making it safe to
+// return, otherwise we panic and possibly uninitialized `v` is not revealed.
+//
+// This only works with POD return types and non-panicing $body, which should always be the case for
+// Perl API.
+macro_rules! xcpt_try {
+    ( $se:ident, $( $body:stmt )* ) => {{
+        let mut v = mem::uninitialized();
+        {
+            let mut callback: &mut FnMut() = &mut || v = { $( $body )* };
+            let rc = ouroboros_xcpt_try($se.0,
+                                        mem::transmute(xcpt_bouncer as extern "C" fn(_)),
+                                        mem::transmute(&mut callback));
+            if rc != 0 {
+                panic!(Xcpt(rc));
+            }
+        }
+        v
+    }}
+}
+
+// Helper function for calling Rust closure from C.
+extern "C" fn xcpt_bouncer(closure: &mut &mut FnMut()) {
+    (*closure)();
+}
+
+macro_rules! perl_api_inner {
+    (
+        pub unsafe fn $name:ident ( & $se:ident, $( $pname:ident : $ptype:ty ),* ) {
+            $( $body:stmt )*
+        }
+        $( $rest:tt )*
+    ) => (
+        perl_api_inner! {
+            pub unsafe fn $name ( & $se, $( $pname : $ptype ),* ) -> () { $( $body )* }
+            $( $rest )*
+        }
+    );
+
+    (
+        pub unsafe fn $name:ident ( & $se:ident, $( $pname:ident : $ptype:ty ),* ) -> $rtype:ty {
+            $( $body:stmt )*
+        }
+        $( $rest:tt )*
+    ) => (
+        pub unsafe fn $name ( & $se, $( $pname : $ptype ),* ) -> $rtype {
+            xcpt_try! { $se, $( $body )* }
+        }
+        perl_api_inner! { $( $rest )* }
+    );
+
+    () => ();
+}
+
+macro_rules! perl_api {
+    ( $( $tt:tt )* ) => ( impl Interpreter { perl_api_inner! { $( $tt )* } } )
+}
+
+perl_api! {
     pub unsafe fn sv_iv(&self, sv: *mut SV) -> IV { ouroboros_sv_iv(self.0, sv) }
     pub unsafe fn sv_uv(&self, sv: *mut SV) -> UV { ouroboros_sv_uv(self.0, sv) }
     pub unsafe fn sv_nv(&self, sv: *mut SV) -> NV { ouroboros_sv_nv(self.0, sv) }

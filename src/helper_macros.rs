1cf54f01a1df0a95ec1faadf6895cd7c1c5bc246
Vickenty Fesunov <kent@setattr.net>
2016-04-18 20:21:11 +0200

    Allow attributes on generated methods.
    Mostly used for documentation attributes.

--- src/helper_macros.rs
+++ src/helper_macros.rs
@@ -1,26 +1,30 @@
 macro_rules! expr { ( $e:expr ) => ( $e ) }
 
 macro_rules! method {
-    (simple fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) $( $rest:tt )* ) => (
+    ($( #[$me:meta] )* simple fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) $( $rest:tt )* ) => (
+        $( #[$me] )*
         pub fn $name(&self, $( $pname : $ptype ),* ) {
             unsafe { self.pthx().$imp(self.as_ptr(), $( $args ),*) };
         }
     );
 
-    (simple fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) -> $rtype:ty = $imp:ident ( $( $args:expr ),* ) $( $rest:tt )* ) => (
+    ($( #[$me:meta] )* simple fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) -> $rtype:ty = $imp:ident ( $( $args:expr ),* ) $( $rest:tt )* ) => (
+        $( #[$me] )*
         pub fn $name(&self, $( $pname : $ptype ),* ) -> $rtype {
             expr! { unsafe { self.pthx().$imp(self.as_ptr(), $( $args ),*) $( $rest )* } }
         }
     );
 
-    (getter fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) ) => (
+    ($( #[$me:meta] )* getter fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) ) => (
+        $( #[$me] )*
         pub fn $name<T>(&self, $( $pname : $ptype ),* ) -> Option<T> where T: $crate::convert::FromSV {
             let svp = unsafe { self.pthx().$imp(self.as_ptr(), $( $args ),*) };
             if !svp.is_null() { Some(unsafe { T::from_sv(self.pthx(), svp) }) } else { None }
         }
     );
 
-    (getptr fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) ) => (
+    ($( #[$me:meta] )* getptr fn $name:ident ( $( $pname:ident : $ptype:ty ),* ) = $imp:ident ( $( $args:expr ),* ) ) => (
+        $( #[$me] )*
         pub fn $name<T>(&self, $( $pname : $ptype ),* ) -> Option<T> where T: $crate::convert::FromSV {
             let svpp = unsafe { self.pthx().$imp(self.as_ptr(), $( $args ),*) };
             if !svpp.is_null() { Some(unsafe { T::from_sv(self.pthx(), *svpp) }) } else { None }

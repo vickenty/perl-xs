cdc6095829593c0fa1ff8e962e71c905ae8db730
Vickenty Fesunov <kent@setattr.net>
2016-11-27 22:52:54 +0100

    Use automatic dereference in benchmark

--- bench/rs/src/lib.rs
+++ bench/rs/src/lib.rs
@@ -3,13 +3,12 @@ extern crate perl_xs;
 #[macro_use]
 extern crate perl_sys;
 
-use perl_xs::{ SV, AV, IV, NV };
+use perl_xs::{ AV, IV, NV };
 
 xs! {
     package XSBench::RS;
     
-    sub rs_sum_loop(ctx, param: SV) {
-        let array: AV = param.deref_av().expect("not an array ref");
+    sub rs_sum_loop(ctx, array: AV) {
         let mut sum = 0.0;
         for i in 0..array.top_index() + 1 {
             if let Some(val) = array.fetch(i) {
@@ -19,8 +18,7 @@ xs! {
         xs_return!(ctx, sum);
     }
 
-    sub rs_sum_iter(ctx, param: SV) {
-        let array: AV = param.deref_av().expect("not an array ref");
+    sub rs_sum_iter(ctx, array: AV) {
         let sum: NV = array.iter().map(|v| v.unwrap_or(0.0)).sum();
         xs_return!(ctx, sum);
     }

ffde5367e049a4b3659624f758ac5268ac21a15f
Vickenty Fesunov <kent@setattr.net>
2016-12-18 10:41:04 +0100

    Add module with croak helpers.
    This file was supposed to be committed with 9639452, but was left out by mistake.

---
+++ src/croak.rs
@@ -0,0 +1,19 @@
+//! Helper functions for the croak! macro.
+
+use std::any::Any;
+use std::fmt::{ Arguments, Write };
+use std::panic;
+
+#[doc(hidden)]
+#[inline(never)]
+pub fn croak<T>(payload: T) -> ! where T: Any + Send {
+    panic::resume_unwind(Box::new(payload));
+}
+
+#[doc(hidden)]
+#[inline(never)]
+pub fn croak_fmt(args: &Arguments) -> ! {
+    let mut s = String::new();
+    let _ = s.write_fmt(*args);
+    croak(s);
+}

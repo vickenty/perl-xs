915d07ba65c9635feb96eb3728971981aac74c0d
Daniel Norman <daniel@danielnorman.net>
2019-08-19 09:59:23 -0700

    rebase bugfixes

--- src/hash.rs
+++ src/hash.rs
@@ -1,12 +1,10 @@
 use std::marker::PhantomData;
-use std::mem;
 use std::slice::from_raw_parts;
 
 use crate::SV;
 use crate::convert::{FromSV, TryFromSV};
 use crate::handle::Owned;
 use crate::raw;
-use crate::SV;
 
 /// Perl hash object.
 pub struct HV(Owned<raw::HV>);
@@ -156,8 +154,8 @@ impl<'a, T: FromSV> Iterator for Iter<'a, T> {
             let pthx = self.hv.pthx();
             let hv_ptr = self.hv.as_ptr();
 
-            let mut k_ptr: *mut i8 = mem::uninitialized();
-            let mut klen: raw::I32 = mem::uninitialized();
+            let mut k_ptr: *mut i8 = std::mem::MaybeUninit::uninit().assume_init();
+            let mut klen: raw::I32 = std::mem::MaybeUninit::uninit().assume_init();
             let v = pthx.hv_iternextsv(
                 hv_ptr,
                 &mut k_ptr as *mut _,
@@ -228,7 +226,7 @@ impl<'a> Iterator for Keys<'a> {
             if he.is_null() {
                 None
             } else {
-                let mut klen: raw::I32 = mem::uninitialized();
+                let mut klen: raw::I32 = std::mem::MaybeUninit::uninit().assume_init();
                 let k_ptr = pthx.hv_iterkey(he, &mut klen as *mut _) as *const u8;
                 let k = from_raw_parts(k_ptr, klen as usize);
                 Some(k)
